42bdb33d2ac542ccfbc98ec93fa9e6bbdab12df8
Niklas Collin <niklas.collin@gmail.com>
2016-02-20 22:39:14 +0200

    working changeset generation and applying across different managers

---
+++ src/puu/model.clj
@@ -0,0 +1,121 @@
+(ns puu.model
+  (:require [clojure.data :as d]
+            [clojure.set :as sets])
+  (:import (clojure.lang Ref IDeref IFn Keyword)
+           (java.lang.ref SoftReference)))
+
+(defprotocol Version
+  (version [_])
+  (timestamp [_])
+  (prev [_]))
+
+(deftype PuuModel [ver ts data ^SoftReference prev]
+  IDeref Version
+  (deref [_] data)
+  (version [_] ver)
+  (timestamp [_] ts)
+  (prev [_] (when (some? prev) (.get prev))))
+
+(defprotocol IManager
+  (previous [_])
+  (ref-access [_])
+  (model-value [_])
+  (mgr-name [_]))
+
+(deftype Manager [mgr-name ^Ref d]
+  IDeref IManager
+  (deref [_] @@d)
+  (model-value [_] @d)
+  (ref-access [_] d)
+  (mgr-name [_] mgr-name)
+  (previous [_] (Manager. mgr-name (ref (prev @d)))))
+
+(defn- new-version [^PuuModel m data]
+  (PuuModel. (inc (version m)) (System/currentTimeMillis) data (SoftReference. m)))
+
+(defn model [m]
+  (PuuModel. 1 (System/currentTimeMillis) m nil))
+
+(defn model->map [^PuuModel m]
+  {:data      @m
+   :version   (version m)
+   :timestamp (timestamp m)})
+
+(defn manager [mgr-name ^PuuModel m]
+  (Manager. mgr-name (ref m :min-history 5 :max-history 100)))
+
+(defn do-tx [^Manager m f]
+  (dosync
+    (alter
+      (ref-access m)
+      (fn [data]
+        (new-version data (f @data))))))
+
+(defn get-version-by-num [mgr version-num]
+  (if-let [cur-version (model-value mgr)]
+    (if (= version-num (version cur-version))
+      cur-version
+      (recur (previous mgr) version-num))
+    (atom nil)))
+
+(defmulti get-version (fn [mgr version-delta] (type version-delta)))
+
+(defmethod get-version IFn [mgr version-delta]
+  (get-version-by-num mgr (version-delta (version (model-value mgr)))))
+
+(defmethod get-version Number [mgr version-delta]
+  (get-version-by-num mgr version-delta))
+
+(defmethod get-version Keyword [mgr version-delta]
+  (condp = version-delta
+    :latest (model-value mgr)
+    :first nil
+    nil))
+
+(defmethod get-version nil [_ _] nil)
+
+(defmethod get-version :default [mgr version-delta]
+  (throw (ex-info
+           (format "Unknown version-delta type: %s" (type version-delta))
+           {:type (type version-delta)})))
+
+(defn version-changes [mgr from to]
+  (let [from-v (get-version mgr from)
+        to-v   (get-version mgr to)
+        diff   (d/diff @from-v @to-v)]
+    {:from    (version from-v)
+     :to      (version to-v)
+     :changes {:additions    (second diff)
+               :subtractions (first diff)}}))
+
+(defn- seqzip
+  "returns a sequence of [[ value-left] [value-right]....]  padding with nulls for shorter sequences "
+  [left right]
+  (loop [list [] a left b right]
+    (if (or (seq a) (seq b))
+      (recur (conj list [(first a) (first b)] ) (rest a) (rest b))
+      list)))
+
+(defn- recursive-diff-merge
+  " Merge two structures recusively , taking non-nil values from sequences and maps and merging sets"
+  [part-state original-state]
+  (cond
+    (sequential? part-state) (map (fn [[l r]] (recursive-diff-merge l r)) (seqzip part-state original-state))
+    (map? part-state) (merge-with recursive-diff-merge part-state original-state)
+    (set? part-state) (sets/union part-state original-state)
+    (nil? part-state ) original-state
+    :default part-state))
+
+(defn undiff
+  "returns the state of x after reversing the changes described by a diff against
+   an earlier state (where before and after are the first two elements of the diff)"
+  [x before after]
+  (let [[a _ _] (d/diff x after)]
+    (recursive-diff-merge a before)))
+
+
+(defn apply-changeset [mgr diff]
+  (do-tx
+    mgr
+    (fn [data]
+      (undiff data (-> diff :changes :additions) (-> diff :changes :subtractions)))))
---
+++ test/puu/model_test.clj
@@ -0,0 +1,136 @@
+(ns puu.model-test
+  (:require [clojure.test :refer :all]
+            [puu.model :refer :all])
+  (:import (java.util.concurrent Executors)))
+
+
+(deftest manager-updates-its-model-and-its-versions
+  (let [mgr (manager "mgr-1" (model {}))]
+    (do-tx mgr (fn [m] (assoc m :a 1)))
+    (is (= {:a 1} @mgr))
+
+    (do-tx mgr (fn [m] (assoc m :b 2)))
+    (is (= {:a 1 :b 2} @mgr))
+
+    (do-tx mgr (fn [m] (assoc m :c 3)))
+    (is (= {:a 1 :b 2 :c 3} @mgr))))
+
+(deftest manager-retains-old-versions
+  (let [m   (model {:value [1]})
+        mgr (manager "mgr-1" m)]
+    (do-tx mgr #(update % :value conj 2))
+    (do-tx mgr #(update % :value conj 3))
+    (do-tx mgr #(update % :value conj 4))
+
+    (is (= [1 2 3 4] (:value @(get-version mgr :latest))))
+    (is (= [1 2 3] (:value @(get-version mgr dec))))
+    (is (= [1 2 3] (:value @(get-version mgr 3))))
+    (is (= [1 2] (:value @(get-version mgr #(- % 2)))))
+    (is (= [1 2] (:value @(get-version mgr 2))))
+    (is (= [1] (:value @(get-version mgr #(- % 3)))))
+    (is (= [1] (:value @(get-version mgr 1))))
+    (is (= nil @(get-version mgr #(- % 4))))
+    (is (= nil @(get-version mgr inc)))))
+
+(deftest manager-handles-concurrent-updates-correctly
+  (let [pool       (Executors/newFixedThreadPool 20)
+        mgr        (manager "mgr-1" (model {:value 0}))
+        tx-counter (atom 0)
+        tasks      (map (fn [n]
+                          (fn []
+                            (dotimes [x 100]
+                              (do-tx mgr (fn [data]
+                                           (swap! tx-counter inc)
+                                           (update data :value inc))))))
+                        (range 100))]
+    (doseq [fut (.invokeAll pool tasks)]
+      (.get fut))
+    (.shutdown pool)
+
+    ; there should have happened 100*100 additions by one to the :value
+    (is (= {:value (* 100 100)} @mgr))
+    ; operation should have caused exactly 100*100 versions to be created
+    (is (= (inc (* 100 100)) (version (model-value mgr))))
+    ; tx-counter should be a LOT higher number than version number since this test causes a lot of
+    ; transaction collisions with extremely high contestion
+    (is (> @tx-counter (version (model-value mgr))))
+    ; history is intact for the latest 100 versions (checking them all would take a lot of time and
+    ; doesn't really add to the test)
+    (doseq [v (range (- (* 100 100) 100) (* 100 100))]
+      (is (= {:value (dec v)} @(get-version mgr v))))))
+
+(deftest map-format-of-model-contains-model-metadata-values
+  (let [m (model {:value [1 2]})]
+    (is (= {:version 1 :timestamp (timestamp m) :data {:value [1 2]}}
+           (model->map m)))))
+
+(deftest calculate-changeset-for-version
+  (let [mgr (manager "mgr-1" (model {:a 1}))]
+    (do-tx mgr (fn [data] (update data :a inc)))
+    (do-tx mgr (fn [data] (assoc-in data [:b] "b")))
+
+    (is (= {:from    2
+            :to      3
+            :changes {:additions    {:b "b"}
+                      :subtractions nil}}
+           (version-changes mgr 2 :latest)))
+    (is (= {:from    1
+            :to      3
+            :changes {:additions    {:a 2 :b "b"}
+                      :subtractions {:a 1}}}
+           (version-changes mgr 1 :latest)))))
+
+(defn two-managers-test [m f]
+  (let [mgr1 (manager "mgr-1" (model m))
+        mgr2 (manager "mgr-2" (model m))]
+
+    (f mgr1 mgr2)
+
+    (apply-changeset mgr2 (version-changes mgr1 1 :latest))
+
+    (is (= @(get-version mgr1 :latest)
+           @(get-version mgr2 :latest)))))
+
+(deftest applying-changeset-gives-identical-model-data-for-other-manager-with-same-base-model
+  (testing "Simple new key"
+    (two-managers-test {:a 1}
+      (fn [mgr1 _] (do-tx mgr1 #(assoc % :b 2)))))
+
+  (testing "Simple key value replacement"
+    (two-managers-test
+      {:a 1}
+      (fn [mgr1 _] (do-tx mgr1 #(assoc % :a 2)))))
+
+  (testing "Simple vector addition"
+    (two-managers-test
+      {:data [1 2]}
+      (fn [mgr1 _] (do-tx mgr1 #(update % :data conj 3)))))
+
+  (testing "Simple vector value removal"
+    (two-managers-test
+      {:data [1 2]}
+      (fn [mgr1 _] (do-tx mgr1 #(update % :data (comp vec drop-last))))))
+
+  (testing "Several keys added in several versions"
+    (two-managers-test
+      {:a 1}
+      (fn [mgr1 _]
+        (do-tx mgr1 #(assoc % :b 2))
+        (do-tx mgr1 #(assoc % :c 3))
+        (do-tx mgr1 #(assoc % :d 4)))))
+
+  (testing "Complex data struture modification"
+    (two-managers-test
+      {:a [1 2] :b {:c "foo" :d 3}}
+      (fn [mgr1 _]
+        (do-tx mgr1 #(update-in % [:b :d] (constantly 1)))
+        (do-tx mgr1 #(update-in % [:a 1] (constantly 10)))
+        (do-tx mgr1 #(assoc-in % [:b :x] {:foo "bar"})))))
+
+  (testing "Both additions and removals in several versions"
+    (two-managers-test
+      {:a [1 2 3]}
+      (fn [mgr1 _]
+        (do-tx mgr1 #(assoc-in % [:b] {:c [4 5 6]}))
+        (do-tx mgr1 #(update-in % [:b :c] conj 7))
+        (do-tx mgr1 #(update-in % [:b] (fn [m] (dissoc m :c))))))))
